4b6836b65017d003f25aa7be65bed4efa6e46d75
SirSaina <32573278+avotra98@users.noreply.github.com>
2025-12-15 19:44:14 +0300

    Add  via n8n automation

---
+++ lib/features/counter/data/counter_notifier.dart
@@ -0,0 +1,13 @@
+```dart
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'counter_notifier.g.dart';
+
+@riverpod
+class CounterNotifier extends _$CounterNotifier {
+  @override
+  int build() => 0;
+
+  void increment() => state++;
+}
+```
